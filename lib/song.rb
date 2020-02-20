99cb011f5ce6df07f5e9a3cc6f113e7b5b951dfa
Timur Mishiev <timurmishiev@gmail.com>
2020-02-20 05:57:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,8 +13,9 @@ def self.all
 end 
 
 def self.new_by_filename(filename)
-  split_filename = filename.split("_")
+  split_filename = filename.split(" _ ")
   song = self.new(split_filename[1])
+  song.artist = 
 end 
 
 end 
